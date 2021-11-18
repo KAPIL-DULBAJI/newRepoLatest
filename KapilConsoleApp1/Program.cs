7c354be7cf44a743533324a79bb7e5ef8101eb6a
kapil <kapildulbaji009@gmail.com>
2021-11-18 23:29:59 +0530

    [kapil] add:added new folders

---
+++ KapilConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapilConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine(" how are you");
+            Console.WriteLine("how is your health");
+            Console.ReadLine();
+        }
+    }
+}
